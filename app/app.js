65d0a49339667a1a86d5269abee946afd58abef2
Hans Maes <SolidSyntax@users.noreply.github.com>
2015-01-13 19:19:02 +0100

    Initial upload - Introduction screen - Basic game-engine - 'Play a practice match' mode

---
+++ app/app.js
@@ -0,0 +1,35 @@
+/*
+ Copyright (c) 2015 "Solid Syntax" <http://www.SolidSyntax.net>
+ Browser game <https://github.com/SolidSyntax/AnimalsAndColors>
+
+ This file is part of AnimalsAndColors.
+
+ AnimalsAndColors is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+'use strict';
+
+angular.module('AnimalsAndColorsApp', [
+    'ngRoute',
+    'ngAnimate',
+    'ngMaterial',
+    'AnimalsAndColorsApp.game',
+    'AnimalsAndColorsApp.title'
+])
+    // Add default route configuration
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .otherwise({redirectTo: '/title'});
+    }])
+;
---
+++ app/game/game.js
@@ -0,0 +1,127 @@
+/*
+ Copyright (c) 2015 "Solid Syntax" <http://www.SolidSyntax.net>
+ Browser game <https://github.com/SolidSyntax/AnimalsAndColors>
+
+ This file is part of AnimalsAndColors.
+
+ AnimalsAndColors is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+'use strict';
+
+angular.module('AnimalsAndColorsApp.game', ['ngRoute', 'ngResource'])
+    // Add route configuration for module
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/game', {
+                templateUrl: 'game/game.html',
+                controller: 'GameCtrl'
+            });
+    }])
+
+    // Add controllers
+    .controller('GameCtrl', ['$scope', '$resource', '$timeout', function ($scope, $resource,$timeout) {
+        // Scope variables
+        $scope.classRightAnimal = "";
+        $scope.classLeftAnimal = "";
+
+        // variables
+        var allQuestions;
+        var selectedQuestions;
+        var currentQuestion;
+        var questionIndex;
+
+        //Scope functions
+        $scope.actionSelectAnswer = function (animal) {
+            $scope.classRightAnimal = "";
+            $scope.classLeftAnimal = "";
+
+            if(animal === currentQuestion.answer.type){
+                alert("Correct!");
+            }else{
+                alert("Wrong!")
+            }
+            $timeout(selectNextQuestion,100);
+        }
+
+
+        //functions
+        var startANewGame = function () {
+            //Clear the current animals
+            $scope.classRightAnimal = "";
+            $scope.classLeftAnimal = "";
+
+            //Select random questions
+            selectedQuestions = selectRandomQuestions(allQuestions, 10);
+
+            //Reset the questionIndex
+            questionIndex = 0;
+
+            //Select the next question
+            selectNextQuestion();
+
+
+        }
+
+        var selectRandomQuestions = function (allQuestions, numberOfQuestions) {
+            //Calculate how many questions to remove
+            var nrOfQuestionsToRemove = allQuestions.length - numberOfQuestions;
+
+            //Create a shallow copy of the original array
+            var result = allQuestions.slice();
+
+            //While there are still elements to remove, remove on random
+            while (nrOfQuestionsToRemove > 0) {
+
+                //Remove one element at a random location
+                result.splice(randomElementIndex, 1);
+                var randomElementIndex = Math.floor(Math.random() * result.length);
+
+                //One less element to remove
+                nrOfQuestionsToRemove--;
+            }
+
+            return result;
+        }
+
+        var selectNextQuestion = function(){
+            //Select from array
+            currentQuestion = selectedQuestions[questionIndex];
+
+            //Set the animals
+            $scope.classLeftAnimal = "leftAnimal " + buildClassStringForAnimal(currentQuestion.leftAnimal);
+            $scope.classRightAnimal = "rightAnimal " + buildClassStringForAnimal(currentQuestion.rightAnimal);
+
+            //Increment the questionIndex
+            questionIndex++;
+        }
+
+        var buildClassStringForAnimal = function (animal){
+            return animal.type + animal.color.charAt(0).toUpperCase() + animal.color.substr(1).toLowerCase();
+        }
+
+
+        //Initialize
+        //Get all questions from a JSON file
+        $resource('resources/questions.json').query().$promise.then(function (result) {
+            // Store for future use
+            allQuestions = result;
+
+            //Start the game !
+            startANewGame();
+
+        });
+
+
+    }]);
